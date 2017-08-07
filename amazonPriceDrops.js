ca67b315261383bf4cc466e6db9b579983ab698d
Matt Busche <mrbusche@gmail.com>
2017-08-06 20:38:41 -0500

    add ability to choose a max price. default is $999,999

--- amazonPriceDrops.js
+++ amazonPriceDrops.js
@@ -1,11 +1,14 @@
 function removeItemsWithoutPriceDrops() {
+  var lowPrice = 999999;
   var anyRemoved = false;
   var listItems = document.getElementsByClassName('a-section g-item-sortable');
 
   for (var i = 0; i < listItems.length; i++) {
     var priceDrop = listItems[i].querySelectorAll('.itemPriceDrop');
+    var price = listItems[i].querySelectorAll('.a-color-price')[0].innerHTML;
+    price = price.replace('$', '');
 
-    if (priceDrop.length == 0) {
+    if (priceDrop.length == 0 || price > lowPrice) {
       listItems[i].parentElement.removeChild(listItems[i]);
       anyRemoved = true;
     }
