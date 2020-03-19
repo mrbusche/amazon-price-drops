f5637012e23ce9d21a0ce8be57e2c320ac095500
Matt Busche <mrbusche@gmail.com>
2020-03-18 21:00:18 -0500

    amazon updated their HTML

--- amazonPriceDrops.js
+++ amazonPriceDrops.js
@@ -1,26 +1,22 @@
-function removeItemsWithoutPriceDrops() {
-  var lowPrice = 999999;
-  var anyRemoved = false;
-  var listItems = document.getElementsByClassName('a-spacing-none g-item-sortable');
+javascript: (function() {
+  function removeItemsWithoutPriceDrops() {
+    const lowPrice = 999999;
+    let anyRemoved = false;
+    const listItems = document.getElementsByClassName('a-spacing-none g-item-sortable');
 
-  for (var i = 0; i < listItems.length; i++) {
-    var priceDrop = listItems[i].querySelectorAll('.itemPriceDrop');
-    var price = listItems[i].querySelectorAll('span.a-offscreen');
-    if (price.length) {
-      price = price[0].innerHTML;
-      price = price.replace('$', '');
-    } else {
-      price = 0;
+    for (var i = 0; i < listItems.length; i++) {
+      let priceDrop = listItems[i].querySelectorAll('span.a-size-small.a-color-tertiary')[1].innerText.startsWith('Price dropped');
+      let price = priceDrop ? listItems[i].querySelectorAll('span.a-offscreen') : 0;
+      if (!priceDrop || price > lowPrice) {
+        listItems[i].parentElement.removeChild(listItems[i]);
+        anyRemoved = true;
+      }
     }
-    if (priceDrop.length == 0 || price > lowPrice) {
-      listItems[i].parentElement.removeChild(listItems[i]);
-      anyRemoved = true;
+
+    if (anyRemoved) {
+      removeItemsWithoutPriceDrops();
     }
   }
 
-  if (anyRemoved) {
-    removeItemsWithoutPriceDrops();
-  }
-}
-
-removeItemsWithoutPriceDrops();
+  removeItemsWithoutPriceDrops();
+})();
