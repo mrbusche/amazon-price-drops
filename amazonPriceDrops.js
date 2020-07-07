a7ffd81de4bfa08c241bd0174ec52db122c9b227
Matt Busche <mrbusche@gmail.com>
2020-07-06 21:56:51 -0500

    New HTML + price drop percent

--- amazonPriceDrops.js
+++ amazonPriceDrops.js
@@ -1,13 +1,17 @@
-javascript: (function() {
+javascript: (function () {
   function removeItemsWithoutPriceDrops() {
     const lowPrice = 999999;
+    const priceDropMin = 1;
     let anyRemoved = false;
     const listItems = document.getElementsByClassName('a-spacing-none g-item-sortable');
 
     for (var i = 0; i < listItems.length; i++) {
-      let priceDrop = listItems[i].querySelectorAll('span.a-size-small.a-color-tertiary')[1].innerText.startsWith('Price dropped');
+      let priceDropSpan = listItems[i].querySelectorAll('span.a-size-base.a-color-base');
+      let priceDrop = priceDropSpan.length > 0 ? priceDropSpan[0].innerText.startsWith('Price dropped') : false;
       let price = priceDrop ? listItems[i].querySelectorAll('span.a-offscreen') : 0;
-      if (!priceDrop || price > lowPrice) {
+      let priceDropPercent = priceDropSpan.length > 0 ? priceDropSpan[0].innerText : '';
+      priceDropPercent = priceDropPercent.replace('Price dropped', '').replace('% since added', '');
+      if (!priceDrop || price > lowPrice || parseInt(priceDropPercent) < priceDropMin) {
         listItems[i].parentElement.removeChild(listItems[i]);
         anyRemoved = true;
       }
