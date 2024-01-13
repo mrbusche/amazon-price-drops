7747a527fb0f391c6f2f7591707e1034d607d5e5
Matt Busche <mrbusche@gmail.com>
2024-01-13 15:19:16 -0600

    count coupon and deal labels (#7)

--- amazonPriceDrops.js
+++ amazonPriceDrops.js
@@ -1,18 +1,16 @@
 javascript: (function () {
   function removeItemsWithoutPriceDrops() {
     const lowPrice = 999999;
-    const priceDropMin = 1;
     let anyRemoved = false;
     const listItems = document.getElementsByClassName('a-spacing-none g-item-sortable');
 
-    for (var i = 0; i < listItems.length; i++) {
-      let priceDropSpan = listItems[i].querySelectorAll('span.a-size-base.a-color-base');
-      let priceDrop = priceDropSpan.length > 0 ? priceDropSpan[0].innerText.startsWith('Price dropped') : false;
-      let price = priceDrop ? listItems[i].querySelectorAll('span.a-offscreen') : 0;
-      let priceDropPercent = priceDropSpan.length > 0 ? priceDropSpan[0].innerText : '';
-      priceDropPercent = priceDropPercent.replace('Price dropped', '').replace('% since added', '');
-      if (!priceDrop || price > lowPrice || parseInt(priceDropPercent) < priceDropMin) {
-        listItems[i].parentElement.removeChild(listItems[i]);
+    for (const element of listItems) {
+      let priceDrop = element.querySelectorAll('span div div div div div div div div div div div.a-row.itemPriceDrop').length;
+      let coupon = element.querySelectorAll('span div div div div div div div div div div div i[id^="coupon-badge"]').length;
+      let dealBadge = element.querySelectorAll('span div div div div div div div div div span.wl-deal-rich-badge').length;
+      let price = priceDrop ? element.querySelectorAll('span.a-offscreen') : 0;
+      if (!(priceDrop || coupon || dealBadge) || price > lowPrice) {
+        element.parentElement.removeChild(element);
         anyRemoved = true;
       }
     }
